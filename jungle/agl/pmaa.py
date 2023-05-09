0b0c09a74f43526a39da9f9d36a5668e020f9e3f
Yannik Marchand <ymarchand@me.com>
2023-05-09 15:11:09 +0200

    pmaa: implement another quirk and fix curve parsing

--- jungle/agl/pmaa.py
+++ jungle/agl/pmaa.py
@@ -9,20 +9,25 @@ MAGIC_NUMBER = struct.unpack(">I", b"PMAA")[0]
 
 
 class StreamIn(streams.StreamIn):
-	"""Memory stream that detects whether strings contain uninitialized bytes.
+	"""Memory stream that detects quirks of Nintendo's tooling.
 
-	In old games, strings with a fixed size contain uninitialized bytes
+	In some games, strings with a fixed size contain uninitialized bytes
 	behind the null terminator. This stream class detects whether that
 	is the case.
+
+	In some games, strings with a fixed size always use the maximum
+	number of bytes. In other games, they only use the necessary
+	number of bytes. This class detects that.
 	"""
 
 	def __init__(self, data, endianness):
 		super().__init__(data, endianness)
+		self.minimize_strings = None
 		self.string_padding = None
 	
-	def fixed_string(self, size):
+	def fixed_string(self, size, max_size=None):
 		data = self.read(size)
-		if b"\0" not in data or data[-1] != 0:
+		if b"\0" not in data:
 			raise ParseError("expected null terminator behind string")
 		
 		string, padding = data.split(b"\0", 1)
@@ -30,29 +35,41 @@ class StreamIn(streams.StreamIn):
 		# Detect whether the memory behind the string
 		# contains uninitialized bytes
 		if len(padding) > 1:
-			if padding[0] != self.string_padding and self.string_padding is not None:
-				raise ParseError("string has inconsistent padding")
 			self.string_padding = padding[0]
 		
+		# Detect whether the string uses the necessary or the
+		# maximum number of bytes.
+		if self.minimize_strings is None and max_size is not None:
+			if size != max_size:
+				self.minimize_strings = True
+			elif padding:
+				self.minimize_strings = False
+
 		return string.decode()
 
 
 class StreamOut(streams.StreamOut):
-	"""Memory stream that writes additional bytes behind a null terminated string.
+	"""Memory stream that reproduces quirks of Nintendo's tooling.
 
-	In old games, strings with a fixed size contain uninitialized bytes
+	In some games, strings with a fixed size contain uninitialized bytes
 	behind the null terminator. This stream class mimics that behavior.
+
+	In some games, strings with a fixed size always use the maximum
+	number of bytes. In other games, they only use the necessary
+	number of bytes. This class mimics the desired behavior.
 	"""
 
-	def __init__(self, endianness, string_padding):
+	def __init__(self, endianness, minimize_strings, string_padding):
 		super().__init__(endianness)
+		self.minimize_strings = minimize_strings or False
 		self.string_padding = string_padding or 0
 	
-	def fixed_string(self, value, size):
+	def fixed_string(self, value, size, allow_minize=True):
 		data = value.encode() + b"\0"
-		data = data.ljust(size - 1, bytes([self.string_padding]))
-		data = data.ljust(size, b"\0")
-		if len(data) != size:
+		if not allow_minize or not self.minimize_strings:
+			data = data.ljust(size - 1, bytes([self.string_padding]))
+			data = data.ljust(size, b"\0")
+		if len(data) > size:
 			raise SaveError("string is too large")
 		self.write(data)
 
@@ -81,6 +98,42 @@ class ParameterType:
 	STRING_REF = 20
 
 
+class CurveType:
+	LINEAR = 0
+	HERMIT = 1
+	STEP = 2
+	SIN = 3
+	COS = 4
+	SIN_POW2 = 5
+	LINEAR_2D = 6
+	HERMIT_2D = 7
+	STEP_2D = 8
+	NONUNIFORM_SPLINE = 9
+	HERMIT_2D_SMOOTH = 10
+
+
+class Curve:
+	def __init__(self):
+		self.type = CurveType.HERMIT_2D
+		self.values = [.0, .0, .5, .5, .5, .5, 1., 1., .5]
+	
+	def parse(self, stream):
+		num_values = stream.u32()
+		if num_values > 30:
+			raise ParseError("curve has too many points")
+
+		self.type = stream.u32()
+		self.values = stream.repeat(stream.float, num_values)
+		stream.skip(4 * (30 - num_values))
+	
+	def save(self, stream):
+		stream.u32(len(self.values))
+		stream.u32(self.type)
+		stream.repeat(self.values, stream.float)
+		for i in range(30 - len(self.values)):
+			stream.float(1.)
+
+
 class Parameter:
 	def __init__(self):
 		self.hash = 0
@@ -88,9 +141,12 @@ class Parameter:
 		self.value = 0
 	
 	def parse(self, stream):
-		end = stream.tell() + stream.u32()
+		base = stream.tell()
+		size = stream.u32()
 		self.type = stream.u32()
 		self.hash = stream.u32()
+
+		data_size = size - 12
 
 		if self.type == ParameterType.BOOL: self.value = stream.bool()
 		elif self.type == ParameterType.F32: self.value = stream.float()
@@ -116,20 +172,33 @@ class Parameter:
 			b = stream.float()
 			a = stream.float()
 			self.value = r, g, b, a
-		elif self.type == ParameterType.STRING32: self.value = stream.fixed_string(32)
-		elif self.type == ParameterType.STRING64: self.value = stream.fixed_string(64)
+		elif self.type == ParameterType.STRING32: self.value = stream.fixed_string(data_size, 32)
+		elif self.type == ParameterType.STRING64: self.value = stream.fixed_string(data_size, 64)
 		elif self.type == ParameterType.CURVE1:
-			self.value = stream.repeat(stream.float, 32)
+			self.value = Curve()
+			self.value.parse(stream)
 		elif self.type == ParameterType.CURVE1:
-			self.value = stream.repeat(stream.float, 64)
+			self.value = []
+			for i in range(2):
+				curve = Curve()
+				curve.parse(stream)
+				self.value.append(curve)
 		elif self.type == ParameterType.CURVE1:
-			self.value = stream.repeat(stream.float, 96)
+			self.value = []
+			for i in range(3):
+				curve = Curve()
+				curve.parse(stream)
+				self.value.append(curve)
 		elif self.type == ParameterType.CURVE4:
-			self.value = stream.repeat(stream.float, 128)
+			self.value = []
+			for i in range(4):
+				curve = Curve()
+				curve.parse(stream)
+				self.value.append(curve)
 		else:
 			raise ParseError("unsupported parameter type: %i" %self.type)
 
-		if stream.tell() != end:
+		if stream.tell() != base + size:
 			raise ParseError("parameter has invalid size")
 
 	def save(self, stream):
@@ -154,10 +223,16 @@ class Parameter:
 				stream.float(self.value[i])
 		elif self.type == ParameterType.STRING32: stream.fixed_string(self.value, 32)
 		elif self.type == ParameterType.STRING64: stream.fixed_string(self.value, 64)
-		elif self.type == ParameterType.CURVE1: stream.repeat(self.value, stream.float)
-		elif self.type == ParameterType.CURVE2: stream.repeat(self.value, stream.float)
-		elif self.type == ParameterType.CURVE3: stream.repeat(self.value, stream.float)
-		elif self.type == ParameterType.CURVE4: stream.repeat(self.value, stream.float)
+		elif self.type == ParameterType.CURVE1: self.value.save(stream)
+		elif self.type == ParameterType.CURVE2:
+			for curve in self.value:
+				curve.save(stream)
+		elif self.type == ParameterType.CURVE3:
+			for curve in self.value:
+				curve.save(stream)
+		elif self.type == ParameterType.CURVE4:
+			for curve in self.value:
+				curve.save(stream)
 		else:
 			raise ParseError("unsupported parameter type: %i" %self.type)
 		
@@ -238,8 +313,11 @@ class PMAAFile:
 		self.effect_version = 0
 		self.effect_type = "aglenv"
 
+		# These are inconsistent quirks of
+		# Nintendo's tooling
 		self.align = True
-		self.string_padding = None
+		self.minimize_strings = False
+		self.string_padding = 0
 
 		self.root = ParameterList()
 
@@ -266,18 +344,19 @@ class PMAAFile:
 		self.effect_version = stream.u32()
 		self.effect_type = stream.fixed_string(stream.u32())
 
-		self.align = stream.tell() & 7 == 0
+		self.align = stream.tell() & 3 == 0
 
 		self.root = ParameterList()
 		self.root.parse(stream)
 
+		self.minimize_strings = stream.minimize_strings
 		self.string_padding = stream.string_padding
 	
 	def save(self):
 		if self.version != 1:
 			raise SaveError("unsupported version number")
 		
-		stream = StreamOut(self.endianness, self.string_padding)
+		stream = StreamOut(self.endianness, self.minimize_strings, self.string_padding)
 		stream.u32(MAGIC_NUMBER)
 		stream.u32(self.version)
 		stream.u32(1 if self.endianness == "<" else 0)
@@ -286,10 +365,10 @@ class PMAAFile:
 
 		type_length = len(self.effect_type) + 1
 		if self.align:
-			type_length = (type_length + 7) & ~7
+			type_length = (type_length + 3) & ~3
 		
 		stream.u32(type_length)
-		stream.fixed_string(self.effect_type, type_length)
+		stream.fixed_string(self.effect_type, type_length, False)
 
 		self.root.save(stream)
 
